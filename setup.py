e6134c36832081851dda86c245fd616e1b7c7ae2
nautiyaldeepak <deepaknautiyal07@outlook.com>
2018-10-12 00:09:59 +0530

    updated setup file

--- setup.py
+++ setup.py
@@ -1,13 +1,9 @@
 from setuptools import setup
 
-with open('README.md', 'r') as fh:
-    long_description = fh.read()
-
 setup(
     name = 'emailpy',
     version = '1.0.0',
     description = 'Send emails from the command line',
-    long_description = long_description,
     author = 'Deepak Nautiyal',
     url = 'https://github.com/nautiyaldeepak/emailpy',
     author_email = 'deepaknautiyal07@outlook.com',
